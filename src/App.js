43b5db58973e73e52afdcb7c951b74d7af8e4ba8
Vali Masca <vali.masca@vali-masca-mbp.home>
2020-03-10 20:26:03 -0700

    building with setState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Person from './Person/Person';
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Vali', age: 38},
+      { name: 'Bethany', age: 26},
+      {name: 'Jacob', age: 33}
+    ]
+  }
+
+  switchNameHandler = () => {
+    //console.log('Was clicked!');
+    // Dont do this: this.state.persons[0].name = "Valer";
+    this.setState({persons: 
+      [
+        { name: 'Valer', age: 38},
+        { name: 'Bethany', age: 26},
+        {name: 'Jacob', age: 34}
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
+        <p>This is really working</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Running</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
 
