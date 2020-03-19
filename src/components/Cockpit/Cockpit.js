e2c6ebbf0cba1b9bfccd14876703124101fd617e
Vali Masca <vali.masca@Vali-Masca-MBP.local>
2020-03-19 14:14:43 -0700

    working with PureComponents and React.memo

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import classes from './Cockpit.css';
 
 const cockpit = (props) => {
-    let assignedClasses = [];
+    useEffect(() => {
+      console.log('[Cockpit.js] useEffect');
+      //Http request...
+      //const timer = 
+      setTimeout(() => {
+        alert('Saved data to Cloud!');
+      }, 1000);
+      return () => {
+        //clearTimeout(timer);
+        console.log('[Cockpit.js] cleanup work in useEffect');
+      };
+    }, []); // this means that useEffect is only triggerd when a person is changed [props.persons]
+// if you want just to run componentDidMount you pass an empty array instead of what we have on line 8
+    
+useEffect(() => {
+  console.log('[Cockipt.js] 2nd useEffect');
+  return () => {
+    console.log('[Cockpit.js] 2nd cleanup work in useEffect');
+  };
+});
+
+
+let assignedClasses = [];
     let btnClass = '';
     if(props.showPersons){
         btnClass = classes.Red;
     }
     
 
-    if (props.persons.length <=2) {
+    if (props.personsLength <=2) {
       assignedClasses.push(classes.red); // classes = ['red']
     }
-    if (props.persons.length <=1) {
+    if (props.personsLength <=1) {
       assignedClasses.push(classes.bold); // classes = ['red' 'bold']
     }
 
@@ -29,4 +51,4 @@ const cockpit = (props) => {
     );
 };
 
-export default cockpit;
+export default React.memo(cockpit);
